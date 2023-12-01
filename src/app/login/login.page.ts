50d35f6f56e7967a3948226cf20343f58e8e6a89
Brett Gibson <brettgibson5@gmail.com>
2023-11-30 23:38:47 -0600

    switched to NgModule

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  // Step 4: Implement the login function
+  login() {
+    if (this.loginForm.valid) {
+      // Perform login action here
+    }
+  }
+}
